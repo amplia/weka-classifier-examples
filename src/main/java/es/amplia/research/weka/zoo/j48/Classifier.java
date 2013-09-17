df421605cc62627ffb94cf2abb6bd3d34930a17b
cbadenes <carlos.badenes@amplia.es>
2013-09-17 15:17:28 +0200

    added tree visualizer

--- src/main/java/es/amplia/research/weka/zoo/j48/Classifier.java
+++ src/main/java/es/amplia/research/weka/zoo/j48/Classifier.java
@@ -1,5 +1,6 @@
 package es.amplia.research.weka.zoo.j48;
 
+import java.awt.BorderLayout;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import weka.classifiers.trees.J48;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
 import es.amplia.research.weka.zoo.AbstractClassifier;
 
 /**
@@ -33,6 +36,24 @@ public class Classifier extends AbstractClassifier {
 		this.tree = (J48) tmp;
 		in.close();
 		logger.info("Loaded model: {}",fileName );		
+	}
+	
+	public void visualize() throws Exception{
+		// display classifier
+	     final javax.swing.JFrame jf = 
+	       new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+	     jf.setSize(500,400);
+	     jf.getContentPane().setLayout(new BorderLayout());
+	     TreeVisualizer tv = new TreeVisualizer(null,((J48)tree).graph(),new PlaceNode2());
+	     jf.getContentPane().add(tv, BorderLayout.CENTER);
+	     jf.addWindowListener(new java.awt.event.WindowAdapter() {
+	       public void windowClosing(java.awt.event.WindowEvent e) {
+	         jf.dispose();
+	       }
+	     });
+	 
+	     jf.setVisible(true);
+	     tv.fitToScreen();
 	}
 
 	
